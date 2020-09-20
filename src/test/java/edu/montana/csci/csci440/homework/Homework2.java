9c47ad187ed511fcb5d701301146a3bd22bb2908
iandeboo1 <49471468+iandeboo1@users.noreply.github.com>
2020-09-20 13:11:26 -0600

    Finished hwk1

--- src/test/java/edu/montana/csci/csci440/homework/Homework2.java
+++ src/test/java/edu/montana/csci/csci440/homework/Homework2.java
@@ -14,7 +14,7 @@ public class Homework2 extends DBTest {
      * Write a query in the string below that returns all artists that have an 'A' in their name
      */
     void selectArtistsWhoseNameHasAnAInIt(){
-        List<Map<String, Object>> results = executeSQL("SELECT * FROM artists");
+        List<Map<String, Object>> results = executeSQL("SELECT Name FROM artists WHERE Name LIKE '%A%'");
         assertEquals(211, results.size());
     }
 
@@ -24,8 +24,7 @@ public class Homework2 extends DBTest {
      */
     void selectAllArtistsWithMoreThanOneAlbum(){
         List<Map<String, Object>> results = executeSQL(
-                "SELECT * FROM artists");
-
+                "SELECT artists.Name, COUNT(DISTINCT albums.AlbumId) as Albums FROM artists JOIN albums ON artists.ArtistId=albums.ArtistId GROUP BY albums.ArtistId HAVING Albums > 1");
         assertEquals(56, results.size());
         assertEquals("AC/DC", results.get(0).get("Name"));
     }
@@ -37,8 +36,8 @@ public class Homework2 extends DBTest {
          */
     void selectTheTrackAndAlbumAndArtistForAllTracksLongerThanSixMinutes() {
         List<Map<String, Object>> results = executeSQL(
-                "SELECT tracks.Name as TrackName, albums.Title as AlbumTitle, artists.Name as ArtistsName FROM tracks " +
-                        "-- NEED TO DO SOME JOINS HERE KIDS");
+                "SELECT tracks.Name as TrackName, albums.Title as AlbumTitle, artists.Name as ArtistsName FROM tracks JOIN albums ON tracks.AlbumId=albums.AlbumId " +
+                        "JOIN artists ON albums.ArtistId=artists.ArtistId WHERE tracks.Milliseconds > 360000");
 
         assertEquals(623, results.size());
 
